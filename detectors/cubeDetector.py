1f56b52050366526248cfcd0cf242a863fda149b
dblincoe <dblincoe@gmail.com>
2020-04-19 18:27:57 -0400

    Speed and power

--- detectors/cubeDetector.py
+++ detectors/cubeDetector.py
@@ -19,25 +19,25 @@ class CubeDetector:
         """
 
         greyscale_img = face_state.face_image.get_greyscale()
+        greyscale_img = cv.GaussianBlur(greyscale_img, (5, 5), 0)
+        greyscale_img = cv.Canny(greyscale_img, 100, 200)
+        greyscale_img = cv.dilate(greyscale_img, np.ones((5, 5)))
+
         best_fit_value = 0
         best_fit_loc = (None, None)
         best_fit_resize = None
 
-        for scale in np.linspace(0.1, 1.0, 30)[::-1]:
+        for scale in np.linspace(0.2, 1.0, 40)[::-1]:
             resized_img = imutils.resize(
                 greyscale_img, width=int(greyscale_img.shape[1] * scale)
             )
             resized_percentage = greyscale_img.shape[1] / float(resized_img.shape[1])
 
             # Break if image is smaller than template
-            if (
-                resized_img.shape[0] < self.template.shape[0]
-                or resized_img.shape[1] < self.template.shape[1]
-            ):
+            if np.any(np.array(resized_img.shape) < np.array(self.template.shape)):
                 break
 
-            edge_img = cv.Canny(resized_img, 100, 200)
-            template_match = cv.matchTemplate(edge_img, self.template, cv.TM_CCOEFF)
+            template_match = cv.matchTemplate(resized_img, self.template, cv.TM_CCOEFF)
 
             (_, maxVal, _, maxLoc) = cv.minMaxLoc(template_match)
 
